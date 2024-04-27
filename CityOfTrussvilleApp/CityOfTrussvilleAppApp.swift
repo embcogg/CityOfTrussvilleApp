59893715434204152baf2c8fca146d602c8c5a97
Emily Coggins <139669664+embcogg@users.noreply.github.com>
2024-04-27 17:54:59 -0500

    Initial Commit

---
+++ CityOfTrussvilleApp/CityOfTrussvilleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityOfTrussvilleAppApp.swift
+//  CityOfTrussvilleApp
+//
+//  Created by Emily Coggins on 4/27/24.
+//
+
+import SwiftUI
+
+@main
+struct CityOfTrussvilleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
